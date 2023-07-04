18b372ce2e34b50c94250a7ebcac671548b0a076
Trần Vương Duy <136442876+tranvuongduy@users.noreply.github.com>
2023-07-05 00:33:22 +0700

    2.8. Data fetching on client side

--- components/common/header.tsx
+++ components/common/header.tsx
@@ -1,7 +1,5 @@
 export interface IHeaderProps {}
 
 export default function Header(props: IHeaderProps) {
-  console.log("header");
-
   return <div>Header</div>;
 }
--- pages/about.tsx
+++ pages/about.tsx
@@ -1,5 +1,7 @@
 import { GetStaticProps } from "next";
 import dynamic from "next/dynamic";
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
 
 export interface AboutPageProps {}
 
@@ -8,11 +10,53 @@ const Header = dynamic(() => import("@/components/common/header"), {
 });
 
 export default function AboutPage(props: AboutPageProps) {
+  const router = useRouter();
+  const [postList, setPostList] = useState([]);
+
+  const page = router.query?.page;
+
+  useEffect(() => {
+    if (!page) return;
+    (async () => {
+      const response = await fetch(
+        `https://js-post-api.herokuapp.com/api/posts?_page=${page}`
+      );
+      const data = await response.json();
+
+      console.log(data);
+
+      setPostList(data.data);
+    })();
+  }, [page]);
+
+  function handleNextClick() {
+    // shallow chỉ cập nhật query chứ không load lại toàn bộ router query
+
+    router.push(
+      {
+        pathname: "/about",
+        query: {
+          page: (Number(page) || 1) + 1,
+        },
+      },
+      undefined,
+      { shallow: true }
+    );
+  }
+
   return (
     <div>
       <h1>About Page</h1>
 
       <Header />
+
+      <ul>
+        {postList.map((post: any) => (
+          <li key={post.id}>{post.title}</li>
+        ))}
+      </ul>
+
+      <button onClick={handleNextClick}>Next page</button>
     </div>
   );
 }
