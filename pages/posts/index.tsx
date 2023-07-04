2d22f244e49c3a109cf9185b9aef0d58f2c051d7
Trần Vương Duy <136442876+tranvuongduy@users.noreply.github.com>
2023-07-04 14:51:24 +0700

    2-6. getStaticProps with Typescript

--- pages/posts/index.tsx
+++ pages/posts/index.tsx
@@ -1,9 +1,41 @@
-import React from "react";
+import { GetStaticProps, GetStaticPropsContext } from "next";
 
-interface IPostsPageProps {}
+interface IPost {
+  id: string;
+  title: string;
+}
 
-const PostsPage: React.FunctionComponent<IPostsPageProps> = (props) => {
-  return <div>Posts Page</div>;
+export interface IPostListPageProps {
+  posts: IPost[];
+}
+
+export default function PostListPage({ posts }: IPostListPageProps) {
+  console.log(posts);
+
+  return (
+    <div>
+      Post List Page
+      <ul>
+        {posts.map((post) => (
+          <li key={post.id}>{post.title}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export const getStaticProps: GetStaticProps = async (
+  context: GetStaticPropsContext
+) => {
+  console.log("static props");
+  const response = await fetch(
+    "https://js-post-api.herokuapp.com/api/posts?_page=1"
+  );
+  const data = await response.json();
+
+  return {
+    props: {
+      posts: data.data.map((item: any) => ({ id: item.id, title: item.title })),
+    },
+  };
 };
-
-export default PostsPage;
